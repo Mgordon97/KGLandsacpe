adba544eda7f6dcb8cda839006a5d08a5973c9cb
Mgordon97 <49401825+Mgordon97@users.noreply.github.com>
2019-04-18 11:56:00 +0100

    Updates

---
+++ precache-manifest.d8912c7ad4117d1b432909c982dc33cc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b0f945f6ddb795fb9db",
+    "url": "/KGLandscape/static/css/main.85ba45ed.chunk.css"
+  },
+  {
+    "revision": "2b0f945f6ddb795fb9db",
+    "url": "/KGLandscape/static/js/main.3202fd5a.chunk.js"
+  },
+  {
+    "revision": "55569a4dff529260fad1",
+    "url": "/KGLandscape/static/js/runtime~main.0aeb4678.js"
+  },
+  {
+    "revision": "80c697a6acc2ba62b4e8",
+    "url": "/KGLandscape/static/js/2.273ae496.chunk.js"
+  },
+  {
+    "revision": "11fbd1cedcdad6fbeb3941ded5338aa4",
+    "url": "/KGLandscape/static/media/CPC Aubergine SKINNY PILLAR Planter (P).11fbd1ce.jpg"
+  },
+  {
+    "revision": "1137acc42219c5e3385e07d6e65091fc",
+    "url": "/KGLandscape/static/media/CPC Yellow Cube Planter.1137acc4.jpg"
+  },
+  {
+    "revision": "e5f77ef356ddfc2afc104bce56085179",
+    "url": "/KGLandscape/static/media/CPC Turquoise COLUMN Planter.e5f77ef3.png"
+  },
+  {
+    "revision": "466c9645e4b48116fb598214bfa38cd4",
+    "url": "/KGLandscape/static/media/CPC Olive Green SKINNY VASE Planter (P).466c9645.jpg"
+  },
+  {
+    "revision": "3704586a62349dbac05964662bf87bd1",
+    "url": "/KGLandscape/static/media/CPC Aubergine SKINNY PILLAR Planter.3704586a.png"
+  },
+  {
+    "revision": "db9051c9a0bf22ba28b2530e827dff23",
+    "url": "/KGLandscape/static/media/CPC Olive Green SKINNY VASE Planter.db9051c9.png"
+  },
+  {
+    "revision": "31fd5ce830958d30050fb8c00440d300",
+    "url": "/KGLandscape/static/media/CPC Turquoise COLUMN Planter (P).31fd5ce8.jpg"
+  },
+  {
+    "revision": "b327726f241a4cda3408fb7cea9da6fb",
+    "url": "/KGLandscape/static/media/Silver Grey COLUMNS (P).b327726f.jpg"
+  },
+  {
+    "revision": "78e074547246a54b3d912f55143d3f0b",
+    "url": "/KGLandscape/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d26febc2ee729af646f9187fcfc2640b.js"
+  "/KGLandscape/precache-manifest.d8912c7ad4117d1b432909c982dc33cc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/KGLandscape/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
