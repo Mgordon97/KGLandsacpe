195f5e7dd8596d7ef6bab7c83fec4d9b3b15d797
Mgordon97 <49401825+Mgordon97@users.noreply.github.com>
2019-04-18 12:08:08 +0100

    fixing homepage routing

--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -7,7 +7,7 @@ class Header extends Component {
      <div className="App">
         <header>
             <div className="logo">
-            <Link to={process.env.PUBLIC_URL + "/"}>Landscape</Link>
+            <Link to={process.env.PUBLIC_URL + "/Homepage"}>Landscape</Link>
             </div>
 
             <nav>
